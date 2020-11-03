761c27408510f8d1036b487db37780b409504c34
stevemurphy47 <doan.nhat61@gmail.com>
2020-11-03 20:20:57 +0700

    update database, add SQL_Insert and product_link

--- sql/CNPM.sql
+++ sql/CNPM.sql
@@ -1,15 +1,15 @@
-CREATE DATABASE CNPM_NHOM_1
+﻿CREATE DATABASE CNPM_NHOM_1
 
 USE CNPM_NHOM_1
 
 CREATE TABLE STAFF
 (
-	id INT NOT NULL UNIQUE IDENTITY,
-	staff_name TEXT,
+	id INT NOT NULL UNIQUE IDENTITY(1,1),
+	staff_name NVARCHAR(50),
 	phone_number VARCHAR(12),
 	date_of_birth DATE,
-	home_address TEXT,
-	citizen_id varchar(12),
+	home_address NVARCHAR(100),
+	citizen_id VARCHAR(12),
 
 	CONSTRAINT pk_staff_id PRIMARY KEY(id)
 );
@@ -17,11 +17,11 @@ GO
 
 CREATE TABLE CUSTOMER
 (
-	id INT NOT NULL UNIQUE IDENTITY,
-	customer_name TEXT,
+	id INT NOT NULL UNIQUE IDENTITY(1,1),
+	customer_name NVARCHAR(50),
 	phone_number VARCHAR(12),
 	date_of_birth DATE,
-	home_address TEXT,
+	home_address NVARCHAR(100),
 
 	CONSTRAINT pk_customer_id PRIMARY KEY(id)
 );
@@ -29,7 +29,7 @@ GO
 
 CREATE TABLE ACCOUNT
 (
-	id INT NOT NULL UNIQUE IDENTITY,
+	id INT NOT NULL UNIQUE IDENTITY(1,1),
 	id_staff INT UNIQUE,
 	username VARCHAR(20) UNIQUE,
 	pass VARCHAR(20),
@@ -40,24 +40,27 @@ CREATE TABLE ACCOUNT
 );
 GO
 
+--id này nên tự nhập tay để đỡ lỗi
 CREATE TABLE PRODUCT_TYPE
 (
-	id INT NOT NULL UNIQUE IDENTITY,
-	product_type_name TEXT,
-	product_type_desc TEXT,
+	id INT NOT NULL UNIQUE, 
+	product_type_name NVARCHAR(50),
+	product_type_desc NVARCHAR(100),
 
 	CONSTRAINT pk_product_type_id PRIMARY KEY(id)
 );
 GO
 
+--gender: 0 = male, 1 = female
 CREATE TABLE PRODUCT
 (
-	id INT NOT NULL UNIQUE IDENTITY,
+	id INT NOT NULL UNIQUE IDENTITY(1,1),
 	id_type INT,
-	product_name TEXT,
+	gender TINYINT DEFAULT(0),
+	product_name NVARCHAR(50),
 	price FLOAT,
-	img_path TEXT,
-	product_desc TEXT,
+	img_path VARCHAR(100),
+	product_desc NVARCHAR(100),
 
 	CONSTRAINT pk_product_id PRIMARY KEY(id),
 	CONSTRAINT fk_product_id_type FOREIGN KEY(id_type) REFERENCES PRODUCT_TYPE(ID)
@@ -76,11 +79,11 @@ GO
 
 CREATE TABLE PURCHASE_BILL
 (
-	id INT NOT NULL UNIQUE IDENTITY,
+	id INT NOT NULL UNIQUE IDENTITY(1,1),
 	id_staff INT,
 	id_customer INT,
 	date_of_purchase DATETIME,
-	purchase_bill_desc TEXT,
+	purchase_bill_desc NVARCHAR(100),
 	total_cost FLOAT,
 
 	CONSTRAINT pk_purchase_bill_id PRIMARY KEY(id),
@@ -100,3 +103,15 @@ CREATE TABLE PURCHASE_BILL_DETAIL
 	CONSTRAINT fk_purchase_bill_detail_id_product FOREIGN KEY(id_product) REFERENCES PRODUCT(id),
 );
 GO
+
+
+/*
+DROP TABLE STORAGE
+DROP TABLE PURCHASE_BILL
+DROP TABLE PURCHASE_BILL_DETAIL
+DROP TABLE PRODUCT_TYPE
+DROP TABLE PRODUCT
+DROP TABLE ACCOUNT
+DROP TABLE CUSTOMER
+DROP TABLE STAFF
+*/
---
+++ sql/SQL_Insert.sql
@@ -0,0 +1,66 @@
+﻿USE CNPM_NHOM_1
+
+--format MM/dd/YYYY
+INSERT INTO STAFF VALUES
+	(N'Dick Thunder', 123456789, '01/01/1991', N'11/11 Something ST., blablahblah, district 9', '111111111'),
+	(N'Nguyễn Văn A', 9876543211, '06/09/1996', N'42/01/25/300/21 Đường Chân Trời, Phường 9, Quận 7', '111111112'),
+	(N'Trần Văn B', 2135656849, '12/21/2001', N'11/11 Something St., blablahblah, district 9', '111111113'),
+	(N'Captain Obvious', 2847591222, '06/15/1995', N'11/11 Something St., blablahblah, district 9', '111111114'),
+	(N'Chad ThunderCock', 4206977713, '06/09/1990', N'11/11 LMAO St., too chad, district 6', '111111115');
+GO
+
+INSERT INTO CUSTOMER VALUES
+	(N'Nguyễn Đức Tài', 0674584643, '01/31/1980', N'581 Annadale Ave.Lorain, OH 44052'),  
+	(N'Santiago Rosario', 0887554322, '04/24/1967', N'8189 Alderwood Road Woodstock, GA 30188'),
+	(N'Abbey Roy', 0777555432, '07/13/1990', N'971 Bridge Drive North Haven, CT 06473'),
+	(N'John Doe', 066546432, '11/21/1979', N'8309 Mountainview Ave.Niles, MI 49120');
+GO
+
+INSERT INTO ACCOUNT VALUES
+	(1, 'dickthunder', 'dickthunder', 0),
+	(2, 'test', '123', 0),
+	(3, 'helloworld', 'helloworld', 0),
+	(null, 'admin', 'admin', 1);
+GO
+
+INSERT INTO PRODUCT_TYPE VALUES
+	(1, N'Architecture', N'LEGO® Architecture presents some of the iconic buildings of world architecture.'),
+	(2, N'City', N'LEGO® City is a realistic LEGO world for your child to explore.'),
+	(3, N'Classic', N'Develop children’s creativity with LEGO® Classic.'),
+	(4, N'Technic™', N'LEGO® Technic provides a real challenge for experienced LEGO builders.');
+GO
+
+-- template: (1,0,N'product_name', 0, '../resouce/lego_placeholder.png', N'desc'),
+INSERT INTO PRODUCT VALUES
+	(1,1,N'Paris', 44.99, '../resouce/lego_placeholder.png', N'Paris'),
+	(1,0,N'London', 44.99, '../resouce/lego_placeholder.png', N'London'),
+	(1,0,N'The White House', 89.99, '../resouce/lego_placeholder.png', N'This LEGO® Architecture display model of The White House.'),
+	(1,0,N'Burj Khalifa', 44.99, '../resouce/lego_placeholder.png', N'Enjoy some quality time recreating a famous Dubai landmark.'),
+	(2,1,N'Ocean Exploration Ship', 124.99, '../resouce/lego_placeholder.png', 'Ocean Exploration Ship'),
+	(2,1,N'Passenger Train', 119.99, '../resouce/lego_placeholder.png', N'Passenger Train'),
+	(2,0,N'Police Station', 89.99, '../resouce/lego_placeholder.png', N'Police Station'),
+	(2,0,N'Tuning Workshop', 89.99, '../resouce/lego_placeholder.png', N'Tuning Workshop'),
+	(3,1,N'LEGO® Large Creative Brick Box', 39.99, '../resouce/lego_placeholder.png', N'LEGO® Large Creative Brick Box'),
+	(3,1,N'Bricks and Lights', 24.99,'../resouce/lego_placeholder.png', N'Bricks and Lights'),
+	(3,1,N'Bricks and Houses', 17.99, '../resouce/lego_placeholder.png', N'Bricks and Houses'),
+	(3,1,N'B6 ricks and Animals', 54.99, '../resouce/lego_placeholder.png', N'Bricks and Animals'),
+	(4,0,N'Land Rover Defender', 154.99, '../resouce/lego_placeholder.png', N'Land Rover Defender'),
+	(4,0,N'Porsche 911 RSR', 89.99, '../resouce/lego_placeholder.png', N'Porsche 911 RSR'),
+	(4,0,N'Dom''s Dodge Charger', 89.99, '../resouce/lego_placeholder.png', N'Dom''s Dodge Charger'),
+	(4,0,N'Ducati Panigale V4 R', 54.99, '../resouce/lego_placeholder.png', N'Ducati Panigale V4 R');
+
+/* SELECT for debugging
+SELECT * FROM STAFF
+SELECT * FROM ACCOUNT
+SELECT * FROM CUSTOMER
+SELECT * FROM PRODUCT
+SELECT * FROM PRODUCT_TYPE
+*/
+
+/* DELETE for debugging
+DELETE FROM ACCOUNT
+DELETE FROM PRODUCT
+DELETE FROM PRODUCT_TYPE
+DELETE FROM STAFF
+DELETE FROM CUSTOMER
+*/
