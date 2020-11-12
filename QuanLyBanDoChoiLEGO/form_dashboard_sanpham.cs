adf9ff6e024e5e26abf73436c1445283bfc98efb
Duong Tien <4401104193@student.hcmue.edu.vn>
2020-11-12 13:59:46 +0700

    properly filled data into datagridview, panel

--- QuanLyBanDoChoiLEGO/form_dashboard_sanpham.cs
+++ QuanLyBanDoChoiLEGO/form_dashboard_sanpham.cs
@@ -20,8 +20,8 @@ namespace QuanLyBanDoChoiLEGO
         private void form_dashboard_sanpham_Load(object sender, EventArgs e)
         {
             // TODO: This line of code loads data into the 'cNPM_NHOM_1DataSet.PRODUCT' table. You can move, or remove it, as needed.
-            this.pRODUCTTableAdapter.Fill(this.cNPM_NHOM_1DataSet.PRODUCT);
-
+            //this.pRODUCTTableAdapter.Fill(this.cNPM_NHOM_1DataSet.PRODUCT);
+            loadDataFromDatabase();
         }
 
         private void btn_add_Click(object sender, EventArgs e)
@@ -65,29 +65,23 @@ namespace QuanLyBanDoChoiLEGO
             CNPM_DataClassesDataContext db = new CNPM_DataClassesDataContext();
             dgv_product.DataSource = db.PRODUCTs.Select(p => new
             {
-                p.id,
-                p.id_type,
-                p.product_name,
-                p.id_age_range,
-                //p.QUYEN,
-                //p.KICHHOAT
+                id = p.id,
+                id_type = p.PRODUCT_TYPE.product_type_name,
+                product_name = p.product_name,
+                id_age_range = p.AGE_RANGE.age_range_name,
+                gender = HelperClass.getGenderString(p.gender),
+                price = p.price,
+                quantity = p.quantity,
+                product_desc = p.product_desc
             });
-            //LoadCboMaTK();
         }
-
+        
         private void displayProductInfo(PRODUCT product)
         {
             textbox_product_name.Text = product.product_name.ToString();
-            textbox_product_type.Text = product.id_type.ToString();
-            //textbox_product_gender.Text = product.gender.ToString();
-            switch (product.gender)
-            {
-                case 0: textbox_product_gender.Text = "Nam"; break;
-                case 1: textbox_product_gender.Text = "Nữ"; break;
-                default: textbox_product_gender.Text = "Khác"; break;
-            }
-            textbox_product_age_range.Text = product.id_age_range.ToString();
-            
+            textbox_product_type.Text = HelperClass.getProductTypeNameString(product.id_type);
+            textbox_product_gender.Text = HelperClass.getGenderString(product.gender);
+            textbox_product_age_range.Text = HelperClass.getAgeRangeString(product.id_age_range);
             textbox_product_quantity.Text = product.quantity.ToString();
             textbox_product_price.Text = product.price.ToString();
             picbox_product.Image = Image.FromFile("../../../" + product.img_path.ToString());
