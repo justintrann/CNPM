b73974cc01a0a1feec979f11c640148b6586e9d7
stevemurphy47 <doan.nhat61@gmail.com>
2020-11-14 19:15:40 +0700

    add import from excel

---
+++ QuanLyBanDoChoiLEGO/form_nhap_hang_2.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace QuanLyBanDoChoiLEGO
+{
+    partial class form_nhap_hang_2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonImport = new System.Windows.Forms.Button();
+            this.comboBoxSheet = new System.Windows.Forms.ComboBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.textBoxDiaChi = new System.Windows.Forms.TextBox();
+            this.labelTenFile = new System.Windows.Forms.Label();
+            this.labelSheet = new System.Windows.Forms.Label();
+            this.dataGridViewSanPham = new System.Windows.Forms.DataGridView();
+            this.labelNhapKho = new System.Windows.Forms.Label();
+            this.cNPM_NHOM_1DataSet = new QuanLyBanDoChoiLEGO.CNPM_NHOM_1DataSet();
+            this.pRODUCTTYPEBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.pRODUCT_TYPETableAdapter = new QuanLyBanDoChoiLEGO.CNPM_NHOM_1DataSetTableAdapters.PRODUCT_TYPETableAdapter();
+            this.pRODUCTBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.pRODUCTTableAdapter = new QuanLyBanDoChoiLEGO.CNPM_NHOM_1DataSetTableAdapters.PRODUCTTableAdapter();
+            this.sTORAGEHISTORYBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.sTORAGE_HISTORYTableAdapter = new QuanLyBanDoChoiLEGO.CNPM_NHOM_1DataSetTableAdapters.STORAGE_HISTORYTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSanPham)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cNPM_NHOM_1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pRODUCTTYPEBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pRODUCTBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sTORAGEHISTORYBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonImport
+            // 
+            this.buttonImport.Location = new System.Drawing.Point(405, 456);
+            this.buttonImport.Name = "buttonImport";
+            this.buttonImport.Size = new System.Drawing.Size(75, 23);
+            this.buttonImport.TabIndex = 15;
+            this.buttonImport.Text = "Import";
+            this.buttonImport.UseVisualStyleBackColor = true;
+            this.buttonImport.Click += new System.EventHandler(this.buttonImport_Click);
+            // 
+            // comboBoxSheet
+            // 
+            this.comboBoxSheet.FormattingEnabled = true;
+            this.comboBoxSheet.Location = new System.Drawing.Point(78, 456);
+            this.comboBoxSheet.Name = "comboBoxSheet";
+            this.comboBoxSheet.Size = new System.Drawing.Size(320, 24);
+            this.comboBoxSheet.TabIndex = 14;
+            this.comboBoxSheet.SelectedIndexChanged += new System.EventHandler(this.comboBoxSheet_SelectedIndexChanged);
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(1029, 428);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowse.TabIndex = 13;
+            this.buttonBrowse.Text = "...";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // textBoxDiaChi
+            // 
+            this.textBoxDiaChi.Location = new System.Drawing.Point(78, 428);
+            this.textBoxDiaChi.Name = "textBoxDiaChi";
+            this.textBoxDiaChi.ReadOnly = true;
+            this.textBoxDiaChi.Size = new System.Drawing.Size(945, 22);
+            this.textBoxDiaChi.TabIndex = 12;
+            // 
+            // labelTenFile
+            // 
+            this.labelTenFile.AutoSize = true;
+            this.labelTenFile.Location = new System.Drawing.Point(9, 428);
+            this.labelTenFile.Name = "labelTenFile";
+            this.labelTenFile.Size = new System.Drawing.Size(59, 17);
+            this.labelTenFile.TabIndex = 11;
+            this.labelTenFile.Text = "Tên File";
+            // 
+            // labelSheet
+            // 
+            this.labelSheet.AutoSize = true;
+            this.labelSheet.Location = new System.Drawing.Point(23, 456);
+            this.labelSheet.Name = "labelSheet";
+            this.labelSheet.Size = new System.Drawing.Size(45, 17);
+            this.labelSheet.TabIndex = 10;
+            this.labelSheet.Text = "Sheet";
+            // 
+            // dataGridViewSanPham
+            // 
+            this.dataGridViewSanPham.AllowUserToDeleteRows = false;
+            this.dataGridViewSanPham.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewSanPham.Location = new System.Drawing.Point(12, 64);
+            this.dataGridViewSanPham.Name = "dataGridViewSanPham";
+            this.dataGridViewSanPham.RowHeadersWidth = 51;
+            this.dataGridViewSanPham.RowTemplate.Height = 24;
+            this.dataGridViewSanPham.Size = new System.Drawing.Size(1092, 355);
+            this.dataGridViewSanPham.TabIndex = 9;
+            // 
+            // labelNhapKho
+            // 
+            this.labelNhapKho.AutoSize = true;
+            this.labelNhapKho.Font = new System.Drawing.Font("Ravie", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNhapKho.Location = new System.Drawing.Point(403, 24);
+            this.labelNhapKho.Name = "labelNhapKho";
+            this.labelNhapKho.Size = new System.Drawing.Size(295, 26);
+            this.labelNhapKho.TabIndex = 8;
+            this.labelNhapKho.Text = "Xin mời nhập món hàng";
+            // 
+            // cNPM_NHOM_1DataSet
+            // 
+            this.cNPM_NHOM_1DataSet.DataSetName = "CNPM_NHOM_1DataSet";
+            this.cNPM_NHOM_1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // pRODUCTTYPEBindingSource
+            // 
+            this.pRODUCTTYPEBindingSource.DataMember = "PRODUCT_TYPE";
+            this.pRODUCTTYPEBindingSource.DataSource = this.cNPM_NHOM_1DataSet;
+            // 
+            // pRODUCT_TYPETableAdapter
+            // 
+            this.pRODUCT_TYPETableAdapter.ClearBeforeFill = true;
+            // 
+            // pRODUCTBindingSource
+            // 
+            this.pRODUCTBindingSource.DataMember = "PRODUCT";
+            this.pRODUCTBindingSource.DataSource = this.cNPM_NHOM_1DataSet;
+            // 
+            // pRODUCTTableAdapter
+            // 
+            this.pRODUCTTableAdapter.ClearBeforeFill = true;
+            // 
+            // sTORAGEHISTORYBindingSource
+            // 
+            this.sTORAGEHISTORYBindingSource.DataMember = "STORAGE_HISTORY";
+            this.sTORAGEHISTORYBindingSource.DataSource = this.cNPM_NHOM_1DataSet;
+            // 
+            // sTORAGE_HISTORYTableAdapter
+            // 
+            this.sTORAGE_HISTORYTableAdapter.ClearBeforeFill = true;
+            // 
+            // form_nhap_hang_2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1114, 493);
+            this.Controls.Add(this.buttonImport);
+            this.Controls.Add(this.comboBoxSheet);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.textBoxDiaChi);
+            this.Controls.Add(this.labelTenFile);
+            this.Controls.Add(this.labelSheet);
+            this.Controls.Add(this.dataGridViewSanPham);
+            this.Controls.Add(this.labelNhapKho);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "form_nhap_hang_2";
+            this.Text = "form_nhap_hang_2";
+            this.Load += new System.EventHandler(this.form_nhap_hang_2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSanPham)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cNPM_NHOM_1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pRODUCTTYPEBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pRODUCTBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sTORAGEHISTORYBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonImport;
+        private System.Windows.Forms.ComboBox comboBoxSheet;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.TextBox textBoxDiaChi;
+        private System.Windows.Forms.Label labelTenFile;
+        private System.Windows.Forms.Label labelSheet;
+        private System.Windows.Forms.DataGridView dataGridViewSanPham;
+        private System.Windows.Forms.Label labelNhapKho;
+        private CNPM_NHOM_1DataSet cNPM_NHOM_1DataSet;
+        private System.Windows.Forms.BindingSource pRODUCTTYPEBindingSource;
+        private CNPM_NHOM_1DataSetTableAdapters.PRODUCT_TYPETableAdapter pRODUCT_TYPETableAdapter;
+        private System.Windows.Forms.BindingSource pRODUCTBindingSource;
+        private CNPM_NHOM_1DataSetTableAdapters.PRODUCTTableAdapter pRODUCTTableAdapter;
+        private System.Windows.Forms.BindingSource sTORAGEHISTORYBindingSource;
+        private CNPM_NHOM_1DataSetTableAdapters.STORAGE_HISTORYTableAdapter sTORAGE_HISTORYTableAdapter;
+    }
+}
---
+++ QuanLyBanDoChoiLEGO/form_nhap_hang_2.cs
@@ -0,0 +1,194 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Z.Dapper.Plus;
+
+namespace QuanLyBanDoChoiLEGO
+{
+    public partial class form_nhap_hang_2 : Form
+    {
+        public form_nhap_hang_2()
+        {
+            InitializeComponent();
+        }
+        private void comboBoxSheet_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable data = tableCollection[comboBoxSheet.SelectedItem.ToString()];
+            //dataGridViewSanPham.DataSource = data;
+            if (data != null)
+            {
+                if (data.TableName == "NhapLoaiSanPham")
+                {
+                    dataGridViewSanPham.DataSource = pRODUCTTYPEBindingSource;
+                    List<PRODUCT_TYPE> product_types = new List<PRODUCT_TYPE>();
+                    for (int i = 0; i < data.Rows.Count; i++)
+                    {
+                        PRODUCT_TYPE product_type = new PRODUCT_TYPE();
+                        product_type.product_type_name = data.Rows[i]["Tên chủ đề sản phẩm"].ToString();
+                        product_type.product_type_desc = data.Rows[i]["Mô tả"].ToString();
+
+                        product_types.Add(product_type);
+                    }
+                    pRODUCTTYPEBindingSource.DataSource = product_types;
+                }
+                else if (data.TableName == "NhapSanPham")
+                {
+                    dataGridViewSanPham.DataSource = pRODUCTBindingSource;
+                    List<PRODUCT> products = new List<PRODUCT>();
+                    for (int i = 0; i < data.Rows.Count; i++)
+                    {
+                        PRODUCT product = new PRODUCT();
+                        product.id_type = Convert.ToByte(data.Rows[i]["ID loại sản phẩm"]);
+                        product.gender = Convert.ToByte(data.Rows[i]["Giới tính"]);
+                        product.id_age_range = Convert.ToByte(data.Rows[i]["Độ tuổi"]);
+                        product.product_name = data.Rows[i]["Tên sản phẩm"].ToString();
+                        product.price = Convert.ToDouble(data.Rows[i]["Đơn giá"]);
+                        product.quantity = Convert.ToInt32(data.Rows[i]["Số lượng"]);
+                        product.img_path = data.Rows[i]["Hình ảnh"].ToString();
+                        product.product_desc = data.Rows[i]["Mô tả"].ToString();
+
+                        products.Add(product);
+                    }
+                    pRODUCTBindingSource.DataSource = products;
+                }
+                else if (data.TableName == "NhapKho")
+                {
+                    dataGridViewSanPham.DataSource = sTORAGEHISTORYBindingSource;
+                    List<STORAGE_HISTORY> storages = new List<STORAGE_HISTORY>();
+                    for (int i = 0; i < data.Rows.Count; i++)
+                    {
+                        STORAGE_HISTORY storage = new STORAGE_HISTORY();
+                        storage.id_product = Convert.ToInt32(data.Rows[i]["ID sản phẩm"]);
+                        storage.input_date = Convert.ToDateTime(data.Rows[i]["Ngày nhập"]);
+                        storage.quantity = Convert.ToInt32(data.Rows[i]["Số lượng"]);
+
+                        storages.Add(storage);
+                    }
+                    sTORAGEHISTORYBindingSource.DataSource = storages;
+                }
+            }
+        }
+        DataTableCollection tableCollection;
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog() { Filter = "Excel 97-2003 Workbook|*.xls|Excel Workbook|*.xlsx" })
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        textBoxDiaChi.Text = openFileDialog.FileName;
+                        using (var stream = File.Open(openFileDialog.FileName, FileMode.Open, FileAccess.Read))
+                        {
+                            using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+                            {
+                                DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                                {
+                                    ConfigureDataTable = (_) => new ExcelDataTableConfiguration() { UseHeaderRow = true }
+                                });
+                                tableCollection = result.Tables;
+                                comboBoxSheet.Items.Clear();
+                                foreach (DataTable table in tableCollection)
+                                {
+                                    comboBoxSheet.Items.Add(table.TableName); //them Sheet vào comboBox
+                                }
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        string title = "Thông báo";
+                        string message = "Không thể thực hiện. Xin hãy đóng file Excel để có thể tiếp tục.";
+                        MessageBox.Show(message, title);
+                        textBoxDiaChi.Text = "";
+                    }
+                }
+            }
+        }
+
+        private void buttonImport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionString = @"Data Source=LAPTOP-C7M7T04D\SQLEXPRESS;Initial Catalog=CNPM_NHOM_1;Integrated Security=True";
+                switch (comboBoxSheet.Text)
+                {
+                    case "NhapLoaiSanPham":
+                        DapperPlusManager.Entity<PRODUCT_TYPE>().Table("PRODUCT_TYPE");
+                        List<PRODUCT_TYPE> product_types = pRODUCTTYPEBindingSource.DataSource as List<PRODUCT_TYPE>;
+                        if (product_types != null)
+                        {
+                            using (SqlConnection db = new SqlConnection(connectionString))
+                            {
+                                db.BulkInsert(product_types);
+                            }
+                        }
+                        MessageBox.Show("Hoàn tất!!!");
+                        break;
+                    case "NhapSanPham":
+                        DapperPlusManager.Entity<PRODUCT>().Table("PRODUCT");
+                        List<PRODUCT> products = pRODUCTBindingSource.DataSource as List<PRODUCT>;
+                        if (products != null)
+                        {
+                            using (SqlConnection db = new SqlConnection(connectionString))
+                            {
+                                db.BulkInsert(products);
+                            }
+                        }
+                        MessageBox.Show("Hoàn tất!!!");
+                        break;
+                    case "NhapKho":
+                        DapperPlusManager.Entity<STORAGE_HISTORY>().Table("STORAGE_HISTORY");
+                        List<STORAGE_HISTORY> storages = sTORAGEHISTORYBindingSource.DataSource as List<STORAGE_HISTORY>;
+                        if (storages != null)
+                        {
+                            using (SqlConnection db = new SqlConnection(connectionString))
+                            {
+                                db.BulkInsert(storages);
+                            }
+                        }
+                        MessageBox.Show("Hoàn tất!!!");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        /*public int IsInt(string input)
+        {
+            int i = 0;
+            string[] numbers = Regex.Split(input, @"\D+");
+            foreach (string value in numbers)
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    i = int.Parse(value);
+                    break;
+                }
+            }
+            return i;
+        }*/
+
+        private void form_nhap_hang_2_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'cNPM_NHOM_1DataSet.STORAGE_HISTORY' table. You can move, or remove it, as needed.
+            this.sTORAGE_HISTORYTableAdapter.Fill(this.cNPM_NHOM_1DataSet.STORAGE_HISTORY);
+            // TODO: This line of code loads data into the 'cNPM_NHOM_1DataSet.PRODUCT' table. You can move, or remove it, as needed.
+            this.pRODUCTTableAdapter.Fill(this.cNPM_NHOM_1DataSet.PRODUCT);
+            // TODO: This line of code loads data into the 'cNPM_NHOM_1DataSet.PRODUCT_TYPE' table. You can move, or remove it, as needed.
+            this.pRODUCT_TYPETableAdapter.Fill(this.cNPM_NHOM_1DataSet.PRODUCT_TYPE);
+
+        }
+    }
+}
