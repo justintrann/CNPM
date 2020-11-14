0efebbba7f7c14ed632760886d9d6680f97b373d
stevemurphy47 <doan.nhat61@gmail.com>
2020-11-14 19:24:28 +0700

    package & config update

--- QuanLyBanDoChoiLEGO/Properties/Settings.Designer.cs
+++ QuanLyBanDoChoiLEGO/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace QuanLyBanDoChoiLEGO.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -30,6 +30,17 @@ namespace QuanLyBanDoChoiLEGO.Properties {
         public string CNPM_NHOM_1ConnectionString {
             get {
                 return ((string)(this["CNPM_NHOM_1ConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-C7M7T04D\\SQLEXPRESS;Initial Catalog=CNPM_NHOM_1;Integrated Sec" +
+            "urity=True")]
+        public string CNPM_NHOM_1ConnectionString1 {
+            get {
+                return ((string)(this["CNPM_NHOM_1ConnectionString1"]));
             }
         }
     }
