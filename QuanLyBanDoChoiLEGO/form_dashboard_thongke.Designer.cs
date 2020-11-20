b6c595488accb28b462915e593cf4f300ffdee99
stevemurphy47 <doan.nhat61@gmail.com>
2020-11-20 11:02:35 +0700

    merge Thongke to program

--- QuanLyBanDoChoiLEGO/form_dashboard_thongke.Designer.cs
+++ QuanLyBanDoChoiLEGO/form_dashboard_thongke.Designer.cs
@@ -28,76 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form_dashboard_thongke));
-            this.domainUpDownThang = new System.Windows.Forms.DomainUpDown();
-            this.domainUpDownNam = new System.Windows.Forms.DomainUpDown();
-            this.chartThongKe = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            ((System.ComponentModel.ISupportInitialize)(this.chartThongKe)).BeginInit();
+            this.NamTextBox = new System.Windows.Forms.TextBox();
+            this.NamLabel = new System.Windows.Forms.Label();
+            this.thgke_button = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.BieuDo = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BieuDo)).BeginInit();
             this.SuspendLayout();
             // 
-            // domainUpDownThang
+            // NamTextBox
             // 
-            this.domainUpDownThang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.domainUpDownThang.Location = new System.Drawing.Point(115, 12);
-            this.domainUpDownThang.Name = "domainUpDownThang";
-            this.domainUpDownThang.Size = new System.Drawing.Size(100, 30);
-            this.domainUpDownThang.TabIndex = 10;
-            this.domainUpDownThang.Text = "Tháng";
+            this.NamTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NamTextBox.ForeColor = System.Drawing.Color.Black;
+            this.NamTextBox.Location = new System.Drawing.Point(77, 5);
+            this.NamTextBox.Multiline = true;
+            this.NamTextBox.Name = "NamTextBox";
+            this.NamTextBox.Size = new System.Drawing.Size(158, 30);
+            this.NamTextBox.TabIndex = 0;
+            this.NamTextBox.TextChanged += new System.EventHandler(this.NamTextBox_TextChanged);
             // 
-            // domainUpDownNam
+            // NamLabel
             // 
-            this.domainUpDownNam.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.domainUpDownNam.Location = new System.Drawing.Point(12, 12);
-            this.domainUpDownNam.Name = "domainUpDownNam";
-            this.domainUpDownNam.Size = new System.Drawing.Size(97, 30);
-            this.domainUpDownNam.TabIndex = 9;
-            this.domainUpDownNam.Text = "Năm";
+            this.NamLabel.AutoSize = true;
+            this.NamLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NamLabel.ForeColor = System.Drawing.Color.Black;
+            this.NamLabel.Location = new System.Drawing.Point(6, 6);
+            this.NamLabel.Name = "NamLabel";
+            this.NamLabel.Size = new System.Drawing.Size(89, 39);
+            this.NamLabel.TabIndex = 1;
+            this.NamLabel.Text = "Năm";
             // 
-            // chartThongKe
+            // thgke_button
             // 
-            this.chartThongKe.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.thgke_button.ForeColor = System.Drawing.Color.Black;
+            this.thgke_button.Location = new System.Drawing.Point(241, 5);
+            this.thgke_button.Name = "thgke_button";
+            this.thgke_button.Size = new System.Drawing.Size(125, 32);
+            this.thgke_button.TabIndex = 2;
+            this.thgke_button.Text = "Thống kê";
+            this.thgke_button.UseVisualStyleBackColor = true;
+            this.thgke_button.Click += new System.EventHandler(this.thgke_button_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.RowHeadersWidth = 51;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.Blue;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.White;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.Size = new System.Drawing.Size(980, 110);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // BieuDo
+            // 
             chartArea1.Name = "ChartArea1";
-            this.chartThongKe.ChartAreas.Add(chartArea1);
+            this.BieuDo.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
-            this.chartThongKe.Legends.Add(legend1);
-            this.chartThongKe.Location = new System.Drawing.Point(12, 48);
-            this.chartThongKe.Name = "chartThongKe";
+            this.BieuDo.Legends.Add(legend1);
+            this.BieuDo.Location = new System.Drawing.Point(12, 157);
+            this.BieuDo.Name = "BieuDo";
             series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.IsValueShownAsLabel = true;
             series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chartThongKe.Series.Add(series1);
-            this.chartThongKe.Size = new System.Drawing.Size(776, 390);
-            this.chartThongKe.TabIndex = 11;
-            this.chartThongKe.Text = "chart1";
+            series1.Name = "Tổng tiền";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Ex";
+            this.BieuDo.Series.Add(series1);
+            this.BieuDo.Series.Add(series2);
+            this.BieuDo.Size = new System.Drawing.Size(980, 380);
+            this.BieuDo.TabIndex = 5;
+            this.BieuDo.Text = "BieuDo";
             // 
             // form_dashboard_thongke
             // 
+            this.AcceptButton = this.thgke_button;
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.chartThongKe);
-            this.Controls.Add(this.domainUpDownThang);
-            this.Controls.Add(this.domainUpDownNam);
+            this.ClientSize = new System.Drawing.Size(1004, 549);
+            this.Controls.Add(this.BieuDo);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.thgke_button);
+            this.Controls.Add(this.NamLabel);
+            this.Controls.Add(this.NamTextBox);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(818, 497);
             this.Name = "form_dashboard_thongke";
             this.ShowIcon = false;
             this.Text = "Thống kê";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            ((System.ComponentModel.ISupportInitialize)(this.chartThongKe)).EndInit();
+            this.Load += new System.EventHandler(this.form_dashboard_thongke_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BieuDo)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DomainUpDown domainUpDownThang;
-        private System.Windows.Forms.DomainUpDown domainUpDownNam;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chartThongKe;
+        private System.Windows.Forms.TextBox NamTextBox;
+        private System.Windows.Forms.Label NamLabel;
+        private System.Windows.Forms.Button thgke_button;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart BieuDo;
+   
     }
 }
--- QuanLyBanDoChoiLEGO/form_dashboard_thongke.cs
+++ QuanLyBanDoChoiLEGO/form_dashboard_thongke.cs
@@ -1,20 +1,148 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace QuanLyBanDoChoiLEGO
 {
     public partial class form_dashboard_thongke : Form
     {
+
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
         public form_dashboard_thongke()
         {
             InitializeComponent();
+            NamTextBox.ForeColor = Color.LightGray;
+            NamTextBox.Text = "Vui lòng nhập năm";
+            this.NamTextBox.Leave += new System.EventHandler(this.NamTextBox_Leave);
+            this.NamTextBox.Enter += new System.EventHandler(this.NamTextBox_Enter);
+
+        }
+
+        private void NamTextBox_Leave(object sender, EventArgs e)
+        {
+            if (NamTextBox.Text == "")
+            {
+                NamTextBox.Text = "Vui lòng nhập năm";
+                NamTextBox.ForeColor = Color.Gray;
+            }
+        }
+
+        private void NamTextBox_Enter(object sender, EventArgs e)
+        {
+            if (NamTextBox.Text == "Vui lòng nhập năm")
+            {
+                NamTextBox.Text = "";
+                NamTextBox.ForeColor = Color.Black;
+            }
+        }
+
+
+        private void NamTextBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void thgke_button_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+
+            //dataGridView1.AutoResizeColumns = true;
+            
+            SqlConnection connection = new SqlConnection(myconnstrng);
+            //string query = "select * from purchase_bill where year(date_of_purchase) = @nam and month(date_of_purchase) = @thang";
+           // string query = "select month(date_of_purchase) as 'Thang', sum(total_cost) as Sum from purchase_bill where year(date_of_purchase) = @nam and month(date_of_purchase) = @thang group by date_of_purchase, total_cost";
+            string query = "select month(date_of_purchase) as Thang, sum(total_cost) as Sum from purchase_bill where year(date_of_purchase) = @nam group by month(date_of_purchase)";
+
+
+            //SqlCommand command = new SqlCommand(query, connection);
+            //command.Parameters.AddWithValue("@nam", nam);
+            // SqlDataAdapter da = new SqlDataAdapter(command);
+
+            
+
+            connection.Open();
+           
+            int nam;
+            
+            bool nam_succeed = Int32.TryParse(NamTextBox.Text, out nam);
+            
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@nam", nam);
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            da.Fill(dt);
+            connection.Close();
+
+            if (nam_succeed == false)
+                MessageBox.Show("Có lỗi trong quá trình sử dụng");
+            else
+            {
+                //clear data
+                foreach (var series in BieuDo.Series)
+                {
+                    series.Points.Clear();
+                }
+                //add data
+                dataGridView1.DataSource = GetAllBill().Tables[0];
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                BieuDo.ChartAreas["ChartArea1"].AxisX.Interval = 1;
+                BieuDo.ChartAreas["ChartArea1"].AxisX.Title = "Tháng";
+                BieuDo.ChartAreas["ChartArea1"].AxisY.Title = "Tổng doanh thu tháng";
+                BieuDo.Series["Tổng tiền"]["DrawingStyle"] = "Cylinder";
+                //if(BieuDo.Series.Contains("Ex"))
+                    //BieuDo.Series.Add("Ex");
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    BieuDo.Series["Tổng tiền"].Points.AddXY(dt.Rows[i]["Thang"], dt.Rows[i]["Sum"]);
+                    BieuDo.Series["Ex"].Points.AddXY(dt.Rows[i]["Thang"], dt.Rows[i]["Sum"]);
+                    BieuDo.Series["Ex"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                }
+            }
+        }
+
+        DataSet GetAllBill()
+        {
+            DataSet data = new DataSet();
+            DataTable dt = new DataTable();
+
+            int nam;
+            bool nam_succeed = Int32.TryParse(NamTextBox.Text, out nam);
+            string query = "select * from purchase_bill where year(date_of_purchase) = @nam";
+            //string query2 = "select month(date_of_purchase) from purchase_bill";
+            
+            using (SqlConnection connection = new SqlConnection(myconnstrng ))
+            {
+                connection.Open();
+
+                //SqlCommand command3 = new SqlCommand(query2, connection);
+
+               
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@nam", nam);
+                SqlDataAdapter da = new SqlDataAdapter(command);
+
+                //SqlDataAdapter chart = new SqlDataAdapter(query2, connection);
+                da.Fill(data);
+                
+                connection.Close();
+            }
+
+            return data;
+        }
+
+        private void form_dashboard_thongke_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
